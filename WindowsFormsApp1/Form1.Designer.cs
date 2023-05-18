064d127ea939b3785416ecec43fdef7c9ab88237
Daria <Daria@DESKTOP-7O8BUGR>
2023-05-19 01:11:08 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SortBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxOperations = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SortBtn
+            // 
+            this.SortBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SortBtn.BackgroundImage")));
+            this.SortBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SortBtn.Location = new System.Drawing.Point(634, 186);
+            this.SortBtn.Name = "SortBtn";
+            this.SortBtn.Size = new System.Drawing.Size(75, 28);
+            this.SortBtn.TabIndex = 0;
+            this.SortBtn.Text = "Sort";
+            this.SortBtn.UseVisualStyleBackColor = true;
+            this.SortBtn.Click += new System.EventHandler(this.SortBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SaveBtn.BackgroundImage")));
+            this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveBtn.Location = new System.Drawing.Point(479, 276);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(77, 33);
+            this.SaveBtn.TabIndex = 2;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LoadBtn.BackgroundImage")));
+            this.LoadBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoadBtn.Location = new System.Drawing.Point(571, 276);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(82, 33);
+            this.LoadBtn.TabIndex = 3;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(199, 168);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(185, 20);
+            this.textBoxInput.TabIndex = 4;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(199, 211);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(185, 20);
+            this.textBoxOutput.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bubble sorting",
+            "Selection sorting",
+            "Insertion sorting",
+            "Quick sorting"});
+            this.comboBox1.Location = new System.Drawing.Point(479, 192);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBoxOperations
+            // 
+            this.textBoxOperations.Location = new System.Drawing.Point(199, 249);
+            this.textBoxOperations.Name = "textBoxOperations";
+            this.textBoxOperations.Size = new System.Drawing.Size(185, 20);
+            this.textBoxOperations.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MistyRose;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(49, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Початковий масив:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MistyRose;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Відсортований масив:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MistyRose;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(53, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Кількість операцій:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Linen;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(475, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Оберіть метод сортування:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOperations);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.SortBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SortBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxOperations;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int operations; // Оголошуємо змінну на рівні класу
+        private int BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            int operations = 0;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    operations++; // Збільшити лічильник операцій
+
+                    if (array[j] > array[j + 1])
+                    {
+                        // Обмін значень між array[j] та array[j + 1]
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            return operations;
+        }
+
+        private int SelectionSort(int[] array)
+        {
+            int n = array.Length;
+            int operations = 0;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    operations++; // Збільшити лічильник операцій
+
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                // Обмін значень між array[i] та array[minIndex]
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+
+            return operations;
+        }
+
+        private int InsertionSort(int[] array)
+        {
+            int n = array.Length;
+            int operations = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                    operations++; // Збільшуємо кількість операцій при здвигу елементів
+                }
+
+                array[j + 1] = key;
+            }
+
+            return operations;
+        }
+
+        private int QuickSort(int[] array, int low, int high, int operations)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high, ref operations);
+                operations = QuickSort(array, low, pivotIndex - 1, operations);
+                operations = QuickSort(array, pivotIndex + 1, high, operations);
+            }
+
+            return operations;
+        }
+
+        private int Partition(int[] array, int low, int high, ref int operations)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                operations++; // Збільшити лічильник операцій
+
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        private int[] GetIntArrayFromTextBox()
+        {
+            // Отримати введений рядок з TextBox
+            string input = textBoxInput.Text;
+
+            // Розділити рядок на окремі числа
+            string[] numbers = input.Split(' ');
+
+            // Конвертувати числа в масив цілих чисел
+            int[] array = Array.ConvertAll(numbers, int.Parse);
+
+            return array;
+        }
+        private int[] GetSortedArrayFromTextBox()
+        {
+            // Отримати введений рядок з TextBox
+            string input = textBoxOutput.Text;
+
+            // Розділити рядок на окремі числа
+            string[] numbers = input.Split(' ');
+
+            // Конвертувати числа в масив цілих чисел
+            int[] array = Array.ConvertAll(numbers, int.Parse);
+
+            return array;
+        }
+
+        private void DisplaySortedArray(int[] array)
+        {
+            // Перетворити відсортований масив на рядок
+            string sortedArrayString = string.Join(" ", array);
+
+            // Присвоїти відсортований рядок властивості Text TextBox
+            textBoxOutput.Text = sortedArrayString;
+        }
+        private void SortBtn_Click(object sender, EventArgs e)
+        {
+            // Очистити текстове поле перед початком роботи
+            textBoxOutput.Clear();
+            textBoxOperations.Clear();
+
+            string selectedSortMethod = comboBox1.SelectedItem.ToString();
+
+            // Оголосити змінну для кількості операцій
+            int operations = 0;
+
+            switch (selectedSortMethod)
+            {
+                case "Bubble sorting":
+                    // Отримати масив з TextBox
+                    int[] array = GetIntArrayFromTextBox();
+
+                    // Викликати метод бульбашкового сортування та отримати кількість операцій
+                    operations = BubbleSort(array);
+
+                    // Вивести відсортований масив
+                    DisplaySortedArray(array);
+
+                    // Вивести кількість операцій
+                    textBoxOperations.Text = $" {operations}";
+
+                    // Очистити змінну після використання, якщо це потрібно
+                    operations = 0;
+                    break;
+                case "Selection sorting":
+                    // Отримати масив з TextBox
+                    int[] array1 = GetIntArrayFromTextBox();
+
+                    // Викликати метод сортування вибором та отримати кількість операцій
+                    operations = SelectionSort(array1);
+
+                    // Вивести відсортований масив
+                    DisplaySortedArray(array1);
+
+                    // Вивести кількість операцій
+                    textBoxOperations.Text = $" {operations}";
+
+                    // Очистити змінну після використання, якщо це потрібно
+                    operations = 0;
+                    break;
+                case "Insertion sorting":
+                    // Отримати масив з TextBox
+                    int[] array2 = GetIntArrayFromTextBox();
+
+                    // Викликати метод сортування вибором та отримати кількість операцій
+                    operations = InsertionSort(array2);
+
+                    // Вивести відсортований масив
+                    DisplaySortedArray(array2);
+
+                    // Вивести кількість операцій
+                    textBoxOperations.Text = $" {operations}";
+
+                    // Очистити змінну після використання, якщо це потрібно
+                    operations = 0;
+                    break;
+                case "Quick sorting":
+                    // Отримати масив з TextBox
+                    int[] array3 = GetIntArrayFromTextBox();
+
+                    // Викликати метод сортування вибором та отримати кількість операцій
+                    operations = QuickSort(array3, 0, array3.Length - 1, operations);
+
+                    // Вивести відсортований масив
+                    DisplaySortedArray(array3);
+                    // Вивести кількість операцій
+                    textBoxOperations.Text = $" {operations}";
+
+                    // Очистити змінну після використання, якщо це потрібно
+                    operations = 0;
+                    break;
+
+            }
+
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+                // Отримати початковий масив з TextBox
+                int[] inputArray = GetIntArrayFromTextBox();
+
+                // Отримати відсортований масив з TextBox
+                int[] sortedArray = GetSortedArrayFromTextBox();
+
+                // Отримати кількість операцій з TextBox
+                string operations = textBoxOperations.Text;
+
+                // Створити вікно вибору місця збереження файлу
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Files|*.txt";
+                saveFileDialog.Title = "Save Results";
+
+                // Відобразити вікно вибору місця збереження
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Отримати шлях до обраного файлу
+                    string filePath = saveFileDialog.FileName;
+
+                    try
+                    {
+                        // Відкрити файл для запису
+                        using (StreamWriter writer = new StreamWriter(filePath))
+                        {
+                            // Записати початковий масив
+                            writer.WriteLine("Початковий масив - " + string.Join(" ", inputArray));
+
+                            // Записати відсортований масив
+                            writer.WriteLine("Відсортований масив - " + string.Join(" ", sortedArray));
+
+                            // Записати кількість операцій
+                            writer.WriteLine(operations);
+                        }
+
+                        MessageBox.Show("Результати збережено у файл " + filePath, "Збережено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Помилка при збереженні результатів: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            // Очистити текстове поле перед початком роботи
+            textBoxInput.Clear();
+            textBoxOutput.Clear();
+            textBoxOperations.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files|*.txt";
+            openFileDialog.Title = "Select File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    string inputArrayLine = "";
+
+                    // Знайти рядок з "Початковий масив"
+                    foreach (string line in lines)
+                    {
+                        if (line.Contains("Початковий масив"))
+                        {
+                            inputArrayLine = line;
+                            break;
+                        }
+                    }
+
+                    // Витягнути масив з рядка
+                    string[] numbers = inputArrayLine.Replace("Початковий масив - ", "").Split(' ');
+                    int[] inputArray = Array.ConvertAll(numbers, int.Parse);
+
+                    textBoxInput.Text = string.Join(" ", inputArray);
+
+                    MessageBox.Show("Масив завантажено з файлу " + filePath, "Завантажено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при завантаженні масиву: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+    }
+
